bda7c1a2dce324b27ea9575216fcdd8878500800
Arnav Choudhury <arnav13@gmail.com>
2019-12-27 18:17:14 +0530

    First Commit

---
+++ App.js
@@ -0,0 +1,31 @@
+import React, { useState } from 'react';
+import { StyleSheet, Text, View, Button } from 'react-native';
+
+export default function App() {
+
+  const [text, updateText] = useState("Click to update text");
+  const [count, updateCount] = useState(1);
+
+  return (
+    <View style={styles.container}>
+      <Text>App.js to start working on your app!</Text>
+      <Button title={ text }
+        onPress={ () => {
+                updateCount( count + 1 );
+                updateText("UPDATED TEXT " + count + " times");
+            }
+        }
+
+      />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
