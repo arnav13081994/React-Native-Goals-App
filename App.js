2bc688724d6b806b9aef9ccbf0e21f7a8e2858d5
Arnav Choudhury <arnav13@gmail.com>
2020-01-13 15:38:36 +0530

    Basic layout rendered

--- App.js
+++ App.js
@@ -1,31 +1,70 @@
 import React, { useState } from 'react';
-import { StyleSheet, Text, View, Button } from 'react-native';
+import { StyleSheet, Text, View, Button, TextInput } from 'react-native';
+// import TextInput from "react-native-web/src/exports/TextInput";
 
 export default function App() {
 
-  const [text, updateText] = useState("Click to update text");
-  const [count, updateCount] = useState(1);
+  const [goal, updateGoal] = useState("");
+  // const [count, updateCount] = useState(1);
 
   return (
-    <View style={styles.container}>
+    <View style={styles.container1}>
+        <View style={styles.container2}>
 
-      <Button title={ text }
-        onPress={ () => {
-                updateCount( count + 1 );
-                updateText("UPDATED TEXT " + count + " times");
-            }
-        }
+            <TextInput style={{ height: 40, borderBottomColor: 'black', borderBottomWidth: 1 }}
+                       value={ goal }
+                       placeholder='  Enter new goal! '
+                       onChangeText={ text => {
+                               updateGoal(text);
+                       }}
+                       onSubmitEditing={ (text, eventCount, target) => {
+                           // <Text> text </Text>
+                           console.log(text);
 
-      />
+
+
+                       }}
+            />
+
+            <Button title='+'
+                    onPress={ () => {
+
+
+
+
+                    }}
+
+
+
+
+            />
+
+        </View>
+
+        <View style={styles.container2}>
+            <Text> NEW GOAL LIST</Text>
+
+
+
+        </View>
+
+
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
+  container1: {
     flex: 1,
     backgroundColor: '#fff',
+    // alignItems: 'center',
+    // justifyContent: 'center',
+  },
+  container2: {
+    flex: 1,
+    flexDirection: 'row',
     alignItems: 'center',
     justifyContent: 'center',
   },
+
 });
