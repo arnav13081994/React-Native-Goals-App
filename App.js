fe61090a95377e3208eef6533aa12d21cf99f71e
Arnav Choudhury <arnav13@gmail.com>
2020-01-14 08:52:53 +0530

    Added a placeholder text when the goals list is empty

--- App.js
+++ App.js
@@ -1,58 +1,44 @@
-import React, { useState } from 'react';
+import React, {useState} from 'react';
 import {StyleSheet, Text, View, Button, TextInput, Alert, FlatList} from 'react-native';
+import GoalItem from "./components/GoalItem";
+import GoalInput from "./components/GoalInput";
+
 
 export default function App() {
 
-	const [goal, updateGoal] = useState("");
 	const [goalList, updateGoalList] = useState([]);
 
-	const goalInputHandler = (newGoal) => {
-	    updateGoal(newGoal)
-    }
-    const addGoalHandler = () => {
-	//    Create a new Text element and append to the view
-        Alert.alert(`Goal added!` );
-        updateGoalList([...goalList, goal]);
-        // Clear the Text input field
-        updateGoal("");
-        // TODO Return focus back to the input field
+	const addGoalHandler = (goalAdded) => {
+		//    Create a new Text element and append to the view
+		Alert.alert(`Goal added!`);
+		updateGoalList([...goalList, goalAdded]);
 
 
-    }
+		// TODO Return focus back to the input field
+	};
 
 	return (
 		<View style={styles.screen}>
-			<View style={styles.GoalInput}>
-
-				<TextInput style={{height: 40, borderBottomColor: 'black', borderBottomWidth: 1, width: '80%'}}
-				           value={goal}
-				           placeholder='  Enter new goal! '
-				           onChangeText={goalInputHandler}
-				           onSubmitEditing={addGoalHandler}
-				/>
-
-				<Button title='+' onPress={addGoalHandler}/>
-
-			</View>
+			{/* Added a custom components for handling GoalInputs */}
+			<GoalInput onAddGoal={addGoalHandler}/>
 
 			<View style={styles.GoalList}>
-				<Text> NEW GOALS:</Text>
-                <FlatList data={goalList}
-                          style={{ contentContainerStyle: 'flex-start', width: '100%'}}
-                          renderItem={ (item) => {
-                                return (
-                                    <View style={styles.GoalListItem}>
-                                        <Text>
-                                            {item.item}
-                                        </Text>
-                                    </View>
-                                )
-                            }
-                          }
-                />
+				<Text> List of Goals:</Text>
+				<FlatList data={goalList}
+				          style={{contentContainerStyle: 'flex-start', width: '100%'}}
+				          ListEmptyComponent={ () => {
+					            return (
+						          <View style={{alignItems: 'center', flex: 1, marginVertical: '45%'}}>
+							          <Text style={{color: 'green'}} >Add a goal, bro!</Text>
+						          </View>
+					            )
+				            }
+				          }
+				          renderItem={(item) => <GoalItem value={item.item}/>}
+					// Added a custom component for rendering Goal Items
+				/>
 
 			</View>
-
 
 		</View>
 	);
@@ -64,22 +50,9 @@ const styles = StyleSheet.create(
 			flex: 1,
 			backgroundColor: '#fff',
 		},
-		GoalInput: {
-			flex: 1,
-			flexDirection: 'row',
-			alignItems: 'center',
-			justifyContent: 'space-evenly',
-		},
 		GoalList: {
 			flex: 5,
 			alignItems: 'center',
 		},
-		GoalListItem: {
-			margin: 10,
-            borderColor: 'black',
-            borderWidth: 1,
-            flex: 1
-		}
-
 	}
 );
